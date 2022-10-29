6195730958209f0ec128d4c1508889e1d06736d8
rafa000costa <rafa000costa@gmail.com>
2022-10-30 00:03:37 +0100

    interação

---
+++ js/homepage.js
@@ -0,0 +1,57 @@
+//OPÇÃO 1
+const opcao1 = document.getElementById('opcao1');
+
+opcao1.addEventListener('click', function onClick(event) {
+  //change background color
+  document.body.style.backgroundColor = '#2B3B89';
+
+  //change text color
+  document.body.style.color = 'white';
+});
+
+//OPÇÃO 2
+const opcao2 = document.getElementById('opcao2');
+
+opcao2.addEventListener('click', function onClick(event) {
+  //change background color
+  document.body.style.backgroundColor = 'black';
+
+  //change text color
+  document.body.style.color = 'white';
+});
+
+
+//OPÇÃO 3
+const opcao3 = document.getElementById('opcao3');
+
+opcao3.addEventListener('click', function onClick(event) {
+  //change background color
+  document.body.style.backgroundColor = '#1F5855';
+
+  //change text color
+  document.body.style.color = 'white';
+});
+
+
+//OPÇÃO 4
+const opcao4 = document.getElementById('opcao4');
+
+opcao4.addEventListener('click', function onClick(event) {
+  //change background color
+  document.body.style.backgroundColor = '#80387B';
+
+  //change text color
+  document.body.style.color = 'white';
+});
+
+
+//OPÇÃO 5
+const opcao5 = document.getElementById('opcao5');
+
+opcao5.addEventListener('click', function onClick(event) {
+  //change background color
+  document.body.style.backgroundColor = '#713D39';
+
+  //change text color
+  document.body.style.color = 'white';
+});
